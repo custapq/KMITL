91b218211e3e8b1d9d0d77a5ef0968e5dc088251
custapq <teeradon88@gmail.com>
2023-07-20 21:11:22 +0700

    update code

--- lab-3/lab3q4_65413.c
+++ lab-3/lab3q4_65413.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main() {
-    int input, sum=0;
+    int input, sum=0 ,count=0;
     while(1){
+		
 	   printf("enter positvie number :");
 	   scanf("%d", &input);
 	   if(input<=0){
 		  break;
  	   }
+	   else{
+		count++;
+	   }
 	  sum += input;
     }
-    printf("Sum : %d", &sum);
+	double average = 0.0;
+	if(count>0){
+		average = (double)(sum/count);
+	}
+    printf("Arverage : %.2f \nSum :%d \n" , average,sum);
+	return 0; 
 }
